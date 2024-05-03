4be01588990e0be6a7ad3bebb90dacc0d3b9b06e
Seif Eldin Gamal <74654269+seifgamal98@users.noreply.github.com>
2024-05-04 02:08:16 +0300

    Latest Commit

--- App.jsx
+++ App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ScrollNavbar from './components/Navbar/ScrollNavbar';
-
+import Craftsmanship from './components/Crafts/Craftsmanship';
+import Experience from './components/experience/Experience';
+import Customization from './components/Customization/Customization';
+import PicSlide from './components/PicSlide/PicSlide'
 const App = () => {
   return (
     <div>
-      <ScrollNavbar /> 
+      <ScrollNavbar />
+      <Craftsmanship/>
+      <Experience/> 
+      <Customization/>
+      <PicSlide/>
+
+
+
       <div style={{ height: '2000px' }}> {/* Example content to allow scrolling */}
-        <h1>Welcome to My Website</h1>
-        <p>Scroll to see the navbar in action.</p>
       </div>
+
     </div>
   );
 };
